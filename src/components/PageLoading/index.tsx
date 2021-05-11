47ec8389adb5b7a510efc32e3bf1a37d3ef1e098
MDMorton6 <43456333+MDMorton6@users.noreply.github.com>
2021-05-11 07:51:13 -0500

    added custom loading page

---
+++ src/components/PageLoading/index.tsx
@@ -0,0 +1,8 @@
+import { PageLoading as DefaultPageLoading} from '@ant-design/pro-layout';
+
+import React from 'react';
+
+// Return value should be component
+const PageLoading = () => <DefaultPageLoading tip="Investri"></DefaultPageLoading>
+
+export default PageLoading;
